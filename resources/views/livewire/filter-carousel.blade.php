a3e3c41192d31fcb8881c1aaa07ff9126820cf30
Sandromaia87 <sandro_maia_mattos@hotmail.com>
2023-01-28 15:12:53 -0300

    commit Initial ilterCarousel

---
+++ resources/views/livewire/filter-carousel.blade.php
@@ -0,0 +1,51 @@
+<div class="row g-2 align-items-center">
+    <div class="col align-content-center">
+        <h2 class="card-subtitle">
+            {{ __('Select Type') }}
+        </h2>
+        <div class="form-selectgroup">
+            <label class="form-selectgroup-item">
+                <input type="radio" name="icons" value="carousel" class="form-selectgroup-input" checked="">
+                <span class="form-selectgroup-label">
+                    <!-- Download SVG icon from http://tabler-icons.io/i/square -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon me-1" width="24" height="24" viewBox="0 0 24 24"
+                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <rect x="4" y="4" width="16" height="16" rx="2">
+                        </rect>
+                    </svg>
+                    Carousel
+                </span>
+            </label>
+            <label class="form-selectgroup-item">
+                <input type="radio" name="icons" value="carouselindicators" class="form-selectgroup-input">
+                <span class="form-selectgroup-label">
+                    <!-- Download SVG icon from http://tabler-icons.io/i/square -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon me-1" width="24" height="24" viewBox="0 0 24 24"
+                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <rect x="4" y="4" width="16" height="16" rx="2">
+                        </rect>
+                    </svg>
+                    Carousel with indicators
+                </span>
+            </label>
+            <label class="form-selectgroup-item">
+                <input type="radio" name="icons" value="carouselcontrols" class="form-selectgroup-input">
+                <span class="form-selectgroup-label">
+                    <!-- Download SVG icon from http://tabler-icons.io/i/square -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon me-1" width="24" height="24" viewBox="0 0 24 24"
+                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <rect x="4" y="4" width="16" height="16" rx="2">
+                        </rect>
+                    </svg>
+                    Carousel with controls
+                </span>
+            </label>
+        </div>
+    </div>
+</div>
